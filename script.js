9d2a3af1798cf37fa706760333021bbb993d16fb
X6parancsnok <33196966+X6parancsnok@users.noreply.github.com>
2019-07-13 02:26:31 +0200

    2.vs

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ var myQuizzes = [
     }
 ];
 
+//console.log(myQuizzes[1].name)
+
 var quizContainer = document.getElementById('quizzes');
 
 showQuizzes(myQuizzes,quizContainer);
@@ -18,40 +20,13 @@ function showQuizzes(quizzes, quizContainer){
     var output = [];
     
     for(var i=0; i<quizzes.length; i++){
-        output.push(
-            '<div class="quiz">' + quizzes[i].name + '</div>'
-        )
-    }
-
-
-/*
-    // for each question...
-    for(var i=0; i<questions.length; i++){
-        
-        // first reset the list of answers
-        answers = [];
-
-        // for each available answer...
-        for(letter in questions[i].answers){
-
-            // ...add an html radio button
-            answers.push(
-                '<label>'
-                    + '<input type="radio" name="question'+i+'" value="'+letter+'">'
-                    + letter + ': '
-                    + questions[i].answers[letter]
-                + '</label>'
-            );
-        }
 
         // add this question and its answers to the output
         output.push(
-            '<div class="question">' + questions[i].question + '</div>'
-            + '<div class="answers">' + answers.join('') + '</div>'
+            '<a href="https://x6parancsnok.github.io/' + quizzes[i].link + 'quiz">' + quizzes[i].name + '</a> </br>' 
         );
     }
-
-    // finally combine our output list into one string of html and put it on the page*/
+    // finally combine our output list into one string of html and put it on the page
     quizContainer.innerHTML = output.join('');
     
 }
